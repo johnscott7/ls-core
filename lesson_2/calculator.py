cd76520e642c015ba76120627ab31e039c2a8358
John Scott <john.scott.seaton@gmail.com>
2026-01-19 15:01:11 -0600

    calculator bonus features

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -4,9 +4,9 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-LANGUAGE = 'ar'
-
 import json
+LANGUAGE = 'en'
+
 with open('calculator_messages.json', 'r') as file:
     MESSAGES = json.load(file)
 
@@ -15,10 +15,9 @@ def prompt(key):
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
-
     return False
 
 prompt('welcome')
@@ -47,13 +46,13 @@ while True:
 
     match operation:
         case '1':
-            output = int(number1) + int(number2)
+            output = float(number1) + float(number2)
         case '2':
-            output = int(number1) - int(number2)
+            output = float(number1) - float(number2)
         case '3':
-            output = int(number1) * int(number2)
+            output = float(number1) * float(number2)
         case '4':
-            output = int(number1) / int(number2)
+            output = float(number1) / float(number2)
 
     prompt('result')
     print(f"==> {output}")
