9e9d8e13722bfc6ed27ba8495e07937b4f372661
John Scott <john.scott.seaton@gmail.com>
2026-02-12 15:48:40 -0600

    small problems started

--- PY110/small_problems/Easy1-02.py
+++ PY110/small_problems/Easy1-02.py
@@ -13,16 +13,20 @@
 # Will want to use floor division (//) to slice the len of the string. 
 # Need to be careful about odd length strings
 
-# not sure how to handle case
+def is_palindrome(my_string):
+    if len(my_string) % 2 == 0:
+        back_half_str = my_string[(len(my_string)//2):]
+        back_half_rev = []
+        for char in back_half_str:
+               back_half_rev.insert(0, char)
+        return "".join(back_half_rev) == my_string[:(len(my_string)//2)]
+    else:
+        back_half_str = my_string[((len(my_string)+1)//2):]
+        back_half_rev = []
+        for char in back_half_str:
+               back_half_rev.insert(0, char)
+        return "".join(back_half_rev) == my_string[:((len(my_string))//2)]
 
-def is_panlindrome(any_string):
-    # Will 
-
-
-
-
-
-# All of these examples should print True
 
 print(is_palindrome('madam') == True)
 print(is_palindrome('356653') == True)
@@ -33,3 +37,18 @@ print(is_palindrome('Madam') == False)
 
 # all characters matter
 print(is_palindrome("madam i'm adam") == False)
+
+# A much, much easier way of doing this is the following:
+def is_palindrome2(s):
+     return s == s[::-1]
+# All of these examples should print True
+
+print(is_palindrome2('madam') == True)
+print(is_palindrome2('356653') == True)
+print(is_palindrome2('356635') == False)
+
+# case matters
+print(is_palindrome2('Madam') == False)
+
+# all characters matter
+print(is_palindrome2("madam i'm adam") == False)
--- PY110/small_problems/Easy1-03.py
+++ PY110/small_problems/Easy1-03.py
@@ -1,29 +1,22 @@
-# Write a function named sum_of_sums that takes a list of numbers and returns the sum
-# of the sums of each leading subsequences in that list
+# Write another function that returns True if the string passed as an argument is a palindrome,
+# or False otherwise. This time, however, your function should be case-insensitive, and should ignore
+# all non-alphanumeric characters.
+def is_real_palindrome(my_string):
+    new_string = ''
+    for char in my_string:
+        if char.isalnum():
+            new_string += char.lower()
+    return new_string == new_string[::-1]
 
-# Examples
-# Input (3, 5, 2) -> Output 21 via (3) + (3 + 5) + (3 + 5 + 2)
-# Input [1, 5, 7, 3] -> Output 36 via (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3)
 
-# Data 
-# Input will be a list of ints
-# Output will be a single int
+print(is_real_palindrome('madam') == True)           # True
+print(is_real_palindrome('356653') == True)          # True
+print(is_real_palindrome('356635') == False)         # True
+print(is_real_palindrome('356a653') == True)         # True
+print(is_real_palindrome('123ab321') == False)       # True
 
-# Algorithm
-# Receive a list passed to the function as an argument
-# Initialize a new variable sum_of_sums and intermediate_sum
-# Iterate through this list, adding each value to intermediate_sum
+# case doesn't matter
+print(is_real_palindrome('Madam') == True)           # True
 
-# Code
-def sum_of_sums(numbers):
-    intermediate_sum = 0
-    total_sum = 0
-    for num in numbers:
-        intermediate_sum += num
-        total_sum += intermediate_sum
-    return total_sum
-
-print(sum_of_sums([3, 5, 2])) # Expected: (3) + (3 + 5) + (3 + 5 + 2) --> 21
-print(sum_of_sums([1, 5, 7, 3])) # Expected: (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
-print(sum_of_sums([4])) # Expected: 4
-print(sum_of_sums([1, 2, 3, 4, 5])) # Expected: 35
+# only alphanumerics matter
+print(is_real_palindrome("Madam, I'm Adam") == True) # True
